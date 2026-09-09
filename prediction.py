86f91e3d9e15bd9b0a369dd6f5c2901d5a974aeb
Minh Nguyen <u3243847@uni.canberra.edu.au>
2026-09-09 17:52:45 +1000

    ada: set threshold 0.7

--- prediction.py
+++ prediction.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 def predict():
-    print("main version")
-    print(2+2)def rmse(y, yhat):
+    print("ada threshold 0.7")
+print(2+2)
+def rmse(y, yhat):
     return ((y - yhat) ** 2).mean() ** 0.5
