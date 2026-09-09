685b057b222b4a1bfd44b9626990966faffe4b3c
Minh Nguyen <u3243847@uni.canberra.edu.au>
2026-09-09 17:54:44 +1000

    bob: set threshold 0.5

--- prediction.py
+++ prediction.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 def predict():
-    print("main version")
-    print(2+2)def rmse(y, yhat):
+    print("bob threshold 0.5")
+print(2+2)
+def rmse(y, yhat):
     return ((y - yhat) ** 2).mean() ** 0.5
